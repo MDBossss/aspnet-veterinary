d65c8d65ac9683a592ef330da98359c8cd0d1697
MDBossss <ferrari020203@gmail.com>
2024-06-01 11:02:30 +0200

    pet filtering

--- veterinary.Web/Controllers/PetController.cs
+++ veterinary.Web/Controllers/PetController.cs
@@ -2,8 +2,10 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 using Veterinary.DAL;
 using Veterinary.Model;
+using Veterinary.Web.Models;
 
 namespace Veterinary.Web.Controllers
 {
@@ -28,5 +30,59 @@ namespace Veterinary.Web.Controllers
 
             return View(pets.ToList());
         }
+
+
+        [Authorize(Roles = RoleConstants.DoctorOrApprentice)]
+        public IActionResult IndexAjax(PetFilterModel petFilterModel)
+        {
+            var pets = veterinaryManagerDbContext.Pets
+                .Include(p => p.Owner)
+                .Include(p => p.Prescriptions)
+                .ToList();
+
+
+            var filteredPets = FilterPets(pets, petFilterModel);
+
+            return PartialView("_IndexTable", filteredPets.ToList());
+        }
+
+        [Authorize(Roles = RoleConstants.DoctorOrApprentice)]
+        [HttpPost]
+        public IActionResult Filter(PetFilterModel petFilterModel)
+        {
+            var pets = veterinaryManagerDbContext.Pets
+                .Include(p => p.Owner)
+                .Include(p => p.Prescriptions)
+                .ToList();
+
+            var filteredPets = FilterPets(pets, petFilterModel);
+
+            return View("Index", pets.ToList());
+        }
+
+        public List<Pet> FilterPets(List<Pet> pets, PetFilterModel filterModel)
+        {
+            // Create a copy of the list to avoid modifying the original list
+            var filteredPets = pets.ToList();
+
+            // Apply filters based on the provided filter model properties
+            if (!string.IsNullOrEmpty(filterModel.Name))
+            {
+                filteredPets = filteredPets.Where(pet => pet.Name.ToLowerInvariant().Contains(filterModel.Name.ToLowerInvariant())).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(filterModel.OwnerName))
+            {
+                filteredPets = filteredPets.Where(pet => pet.Owner.FullName.ToLowerInvariant().Contains(filterModel.OwnerName.ToLowerInvariant())).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(filterModel.Species))
+            {
+                filteredPets = filteredPets.Where(pet => pet.Species.ToLowerInvariant().Contains(filterModel.Species.ToLowerInvariant())).ToList();
+            }
+
+            return filteredPets;
+        }
+
     }
 }
---
+++ veterinary.Web/Models/PetFilterModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Veterinary.Web.Models;
+
+public record PetFilterModel(
+
+    string Name = "",
+    string OwnerName = "",
+    string Species = ""
+    );
